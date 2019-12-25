c83fb0a6fbc6bc94469bba69b6970b996f244a09
Amenates <iAmenates@gmail.com>
2019-12-25 22:58:14 +0300

    add 04

--- 04_homework/script.js
+++ 04_homework/script.js
@@ -1,8 +1,18 @@
 // Task 1
-// Создайте button - при нажатии на него выводите alert с номером задачи.
+// Создайте button - при нажатии на него выводите alert с номером задачи
 
 function f1() {
     alert('Task 1');
 }
 
 document.querySelector('.b-1').onclick = f1;
+
+// Task 2
+// Создайте input type=button - при нажатии на него выводите alert
+
+
+function f2() {
+    alert('Task 2');
+}
+
+document.querySelector('.i-1').onclick = f2;
