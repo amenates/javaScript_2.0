6bbcc2d941b4f42f0e298d8da892e24d0f3377bc
Julia Nikolaeva <julia.nikolaeva@myoffice.team>
2020-01-17 18:43:55 +0300

    add test

---
+++ js_brackets/script.js
@@ -0,0 +1,8 @@
+let int = '()((';
+
+console.log(typeof int); // узнаем тип переменной
+console.log(int.length); // узнаем длинну строки
+
+for (let i = int.length; i >= '('; i++) {
+    console.log('ура');
+}
