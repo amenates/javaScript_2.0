13eacdfc26ad298df5a0c2ed78bdd50cbe8eeead
Amenates <iamenates@gmail.com>
2020-05-25 10:23:07 +0300

    add homework unit 09

--- 00_homeworks/09_homework/unit_09.js
+++ 00_homeworks/09_homework/unit_09.js
@@ -1,7 +1,8 @@
 //  Task 1
 // Добавьте кнопку .b-1, которая запускает функцию f1. Функция присваивает блоку .out-1 ширину 200px, высоту 40px.
 
-const out1 = document.querySelector('.out-1');
+let out1 = document.querySelector('.out-1');
+
 function f1() {
     out1.style.width = '200px';
     out1.style.height = '40px';
@@ -12,7 +13,8 @@ document.querySelector('.b-1').onclick = f1;
 //  Task 2
 // По нажатию кнопки b-2 запускайте функцию f2, которая присваивает блоку out-2 класс .bg-2.
 
-const out2 = document.querySelector('.out-2');
+let out2 = document.querySelector('.out-2');
+
 function f2() {
     out2.classList.add('bg-2');
 }
@@ -23,7 +25,8 @@ document.querySelector('.b-2').onclick = f2;
 //  Task 3
 // По нажатию кнопки b-3 запускайте функцию f3, которая удаляет у блока out-3 класс .bg-3.
 
-const out3 = document.querySelector('.out-3');
+let out3 = document.querySelector('.out-3');
+
 function f3() {
     out3.classList.remove('bg-3');
 }
@@ -33,7 +36,8 @@ document.querySelector('.b-3').onclick = f3;
 //  Task 4
 // По нажатию кнопки b-4 запускайте функцию f4, которая делает toggle класса bg-4 для блока out-4.
 
-const out4 = document.querySelector('.out-4');
+let out4 = document.querySelector('.out-4');
+
 function f4() {
     out4.classList.toggle('bg-4');
 }
@@ -44,7 +48,8 @@ document.querySelector('.b-4').onclick = f4;
 //  Task 5
 // По нажатию b-5 запускайте функцию f5, которая проверяет наличие класса bg-4 у блока out-4 (да, именно bg-4 у out-4 ). Результат - true или false, выводите в out-5.
 
-const out5 = document.querySelector('.out-5');
+let out5 = document.querySelector('.out-5');
+
 function f5() {
     if (out4.classList.contains('bg-4')) {
         out5.textContent = 'true';
@@ -60,10 +65,10 @@ document.querySelector('.b-5').onclick = f5;
 //  Task 6
 // По нажатию b-6 запускайте функцию f6, которая выводит в out-6 количество параграфов с классом p-6.
 
-const out6 = document.querySelector('.out-6');
+let out6 = document.querySelector('.out-6');
+
 function f6() {
-    let a = document.getElementsByClassName('p-6').length;
-    out6.textContent = a;
+    out6.textContent = document.getElementsByClassName('p-6').length;
 }
 
 document.querySelector('.b-6').onclick = f6;
@@ -75,7 +80,9 @@ document.querySelector('.b-6').onclick = f6;
 let blocks7 = document.querySelectorAll('.out-7');
 
 function f7() {
-    //внутри цикла blocks7[i].classList....
+    for (let i = 0; i < blocks7.length; i++) {
+        blocks7[i].classList.add('bg-7');
+    }
 }
 
 document.querySelector('.b-7').onclick = f7;
@@ -87,7 +94,9 @@ document.querySelector('.b-7').onclick = f7;
 let blocks8 = document.querySelectorAll('.out-8');
 
 function f8() {
-    //внутри цикла blocks8[i].classList....
+    for (let i = 0; i < blocks8.length; i++) {
+        blocks8[i].classList.toggle('bg-8');
+    }
 }
 
 document.querySelector('.b-8').onclick = f8;
@@ -97,7 +106,7 @@ document.querySelector('.b-8').onclick = f8;
 // Усложним предыдущие задачи. С помощью цикла повесьте на блоки out-9 событие клик. По клику должна выполняться функция f9. Функция, должна добавлять класс bg-9 тому out-9 на котором кликнули.
 
 function f9() {
-    //this.classList...  // все решается одной строкой
+    this.classList.toggle('bg-9');
 }
 
 let div9 = document.querySelectorAll('.out-9');
@@ -110,14 +119,15 @@ for (let i = 0; i < div9.length; i++) {
 //  Task 10
 // Усложним предыдущие задачи. С помощью цикла повесьте на блоки out-10 событие клик. По клику должна выполняться функция f10. Функция, должна делать toggle класса bg-10 тому out-10 на котором кликнули.
 
-//let div10 = тут получите все out-10
+let div10 = document.querySelectorAll('.out-10');
 
 function f10() {
-
+    this.classList.toggle('bg-10');
 }
 
-// а тут цикл, похожий на предыдущее задание
-
+for (let i = 0; i < div10.length; i++) {
+    div10[i].onclick = f10;
+}
 
 
 //  Task 11
