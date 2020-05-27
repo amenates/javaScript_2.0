cf3c93d5901d72bde14c9e5951d1fdf15ec4d673
Julia Nikolaeva <julia.nikolaeva@myoffice.team>
2020-05-28 01:08:05 +0300

    решение задачи про скобки

--- js_brackets/script.js
+++ js_brackets/script.js
@@ -1,8 +1,20 @@
-let int = ['()(('];
-
-console.log(typeof int); // узнаем тип переменной
-console.log(int.length); // узнаем длинну строки
-
-for (let i = int.length; i >= '('; i++) {
-    console.log('ура');
+let int = '()()()((((()()))))())(';
+let out = '';
+// цикл
+// длина строки
+let count = 0;
+for (let i = 0; i < int.length; i++) {
+    if (int[i] == '(') count++; // увеличиваем на единицу
+    if (int[i] == ')') count--; // уменьшает на единицу
+    if (count < 0) break;
 }
+
+if (count != 0) out = `не правильно`;
+else out = `правильно`;
+
+document.querySelector('.out').textContent += out;
+
+// усложнить задачу
+// input принимает скобочки, и выводит правильно не правильно ввели
+// запуск по кнопке
+// выод на страницу
